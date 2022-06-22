a14a41a4bc2d926a5a272573ae91d0eaadaf87f4
Papazsende <bariscetintr@gmail.com>
2022-06-22 21:52:35 +0300

    inheritance added

--- app/src/main/java/cetin/baris/kotlinbasics/OOP-basics.kt
+++ app/src/main/java/cetin/baris/kotlinbasics/OOP-basics.kt
@@ -2,35 +2,40 @@ package cetin.baris.kotlinbasics
 
 fun main(){
 
-    var myPhone1 = MobilePhone("Android","Samsung","A50","Broken")
-    var myPhone2 = MobilePhone("Android","Samsung","A70",null)
-    var myPhone3 = MobilePhone("iOS","Apple","iphone X",null)
-
-    myPhone2.showState()
-    myPhone3.showState()
+    var myPhone1 = mobilePhone("Android","Samsung","A50",null)
+    myPhone1.charge(30)
     myPhone1.showState()
-
+    myPhone1.brakePhone()
+    myPhone1.showState()
     myPhone1.fixPhone()
-    //myPhone1.state = "rusty" since state variable is private, we cannot reach it from main class
     myPhone1.showState()
-
-
 }
 
-class MobilePhone constructor(osName : String, brand : String, model : String, state:String?){
+open class mobilePhone constructor(osName : String, brand : String, model : String, state:String?){
     var osName : String? = null
+    var battery : Int = 50
     init{
         println("a new mobile phone created os name : $osName brand : $brand  model : $model")
         this.osName = osName
+
     }
     private var state : String? = state ?: "good condition"
     var model : String = model
+
+    fun charge(amount : Int){
+        battery += amount
+        println("Phone is charged from ${battery-amount} to $battery")
+    }
+
     fun showState(){
         println(" $osName phone $model is $state")
     }
 
     fun fixPhone(){
         this.state = "fixed"
+    }
+    fun brakePhone(){
+        this.state = "shattered"
     }
     //or instead of using a method to fix phone we can reach out the member variable state and modify it; see line 14
 
--- app/src/main/java/cetin/baris/kotlinbasics/OOP-inheritance.kt
+++ app/src/main/java/cetin/baris/kotlinbasics/OOP-inheritance.kt
@@ -2,18 +2,49 @@ package cetin.baris.kotlinbasics
 
 fun main(){
 
-    var myCar = car(1200,"Volkswagen","White")
+    var myCar = car("Auidi","R8")
+    var teslaS = electricCar("Tesla","Model S",20.0)
+
+    teslaS.drive(10.0)
+    println("battery is now at ${teslaS.battery}%")
 }
 
-class car constructor(engineCc : Int, brand : String, Color : String ){
+open class car(brand : String,model : String){
 
-    init{
-        println("car object created! Engine size : $engineCc cc , Brand : $brand , Color : $Color")
+    var model = model
+    val mpg = 13
+    var fuel : Double = 0.0
+    open var range : Double = mpg * fuel
 
+    open fun takeFuel(amount : Double){
+        fuel += amount
+        range = mpg * fuel
+        println("${fuel} gallons of Fuel loaded new range is $range miles")
+    }
+
+    fun drive(miles : Double){
+        range -= miles
+        println("$model is driven $miles miles ,new range is : $range ")
+
+    }
+
+    init {
+        println("a car object created! Brand $brand $model")
     }
 
 }
 
-class Volkswagen (engineCc : String, brand : String) : car(engineCc,brand){
+class electricCar(brand: String,model: String,battery : Double) : car(brand,model){
+
+    override var range = battery * 6
+    var battery : Double = battery
+
+    override fun takeFuel(amount: Double){
+        battery += amount
+        println("battery is charged $battery")
+        range = battery * 6
+
+    }
+
 
 }
